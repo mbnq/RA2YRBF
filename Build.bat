639b4c2beb93757bdef168378fa702e734706d2e
MahBoiDeveloper <mah.boi.developer@gmail.com>
2024-04-11 20:14:39 +0300

    Simplify compiling script

--- Build.bat
+++ Build.bat
@@ -1,7 +1,14 @@
 @echo off
 chcp 65001 > nul
-echo Compiling mix archives...
-Tools\make.exe -f Tools\makefile
-echo.
-echo Done.
+
+echo Compiling mix files...
+
+for /f "tokens=*" %%f in ('dir "MIX\" /a:d /b') do (
+	echo Compiling %%f.mix...
+	Tools\ccmix\ccmix.exe --create --lmd --game=ts --dir "MIX\%%f" --mix "%%f.mix"
+	echo.
+	)
+
+set tmp=%0
+echo Done. Check mix files in [%tmp:~1,-11%] folder.
 pause
