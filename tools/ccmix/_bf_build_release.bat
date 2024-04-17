e11e4b63a2fb264bb89c6ec2a9632ad9c4c3ab8a
mbnq <mbnq00@gmail.com>
2024-04-17 11:03:31 +0200

    0.8.326
    - added _bf_build_release.bat, should be used in parent directory of RA2YRBF

---
+++ tools/ccmix/_bf_build_release.bat
@@ -0,0 +1,41 @@
+REM mbnq00@gmail.com
+REM https://www.mbnq.pl/
+
+@echo off
+title Mbnq's Brute Force Release Builder
+setlocal
+cls
+
+call :intro
+
+if NOT exist Rar.exe (echo:Rar.exe not found&&goto bye)
+
+set "RarPath=Rar.exe"
+set "verPreffix=RA2YRBF_0_8_"
+set "SourceDir=F:\projekty\_cnc_\RA2BruteForce\RA2BF\RA2YRBF"
+
+set /p ArchiveName=Name your release suffix (preffix is "%verPreffix%"): 
+set "OutputArchive=%verPreffix%%ArchiveName%.zip"
+
+"%RarPath%" a -r -ep1 "%OutputArchive%" "%SourceDir%" -z"%SourceDir%\README.md" -x"%SourceDir%\_build.bat" -x"%SourceDir%\MIX" -x"%SourceDir%\tools\ccmix" -x"%SourceDir%\.git" -x"%SourceDir%\.gitattributes" -x"%SourceDir%\.gitignore"
+
+echo Archive ready: %OutputArchive%
+goto bye
+
+:bye
+	echo:
+	echo:Press any key to quit this uninstaller...
+	pause > nul
+endlocal	
+exit
+
+:intro
+	cls
+	echo:*********************************************************
+	echo:*                                                       *
+	echo:*  Command ^& Conquer Yuri^'s Revenge - Brute Force Mod   *
+	echo:*                                                       *
+	echo:*                                        mbnq.pl 2024   *
+	echo:*                                                       *
+	echo:*********************************************************
+	echo:
