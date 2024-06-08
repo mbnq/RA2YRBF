470497affcbd8e62da2a84ef0bbd2e1fb57b7750
mbnq <mbnq00@gmail.com>
2024-06-08 12:50:04 +0200

    0.8.4506
    - added bf_downloadLatest2.ps1

---
+++ tools/bf_tools/OtherScripts/bf_downloadLatest2.ps1
@@ -0,0 +1,61 @@
+<#
+	 ____             _         ______                 
+	 |  _ \           | |       |  ____|                
+	 | |_) |_ __ _   _| |_ ___  | |__ ___  _ __ ___ ___ 
+	 |  _ <| '__| | | | __/ _ \ |  __/ _ \| '__/ __/ _ \
+	 | |_) | |  | |_| | ||  __/ | | | (_) | | | (_|  __/
+	 |____/|_|   \__,_|\__\___| |_|  \___/|_|  \___\___|
+														
+	 mbnq00@gmail.com
+	 https://www.mbnq.pl/
+
+
+	 .ini 	Version: 0.8.4506
+	  mod	Version: 0.8.4506
+
+	 This script will download latest push from GitHub repo.
+	 Simplified version for batch script usage.
+	_____________________________________________________________________________________
+#>
+
+$repo = "mbnq/RA2YRBF"
+$textRepo = "GitHub/"+$repo
+
+Write-Host "This script will download latest .zip release from: $textRepo"
+
+$releaseInfo = Invoke-RestMethod -Uri "https://api.github.com/repos/$repo/releases"
+
+if ($releaseInfo -eq $null -or $releaseInfo.Count -eq 0) {
+    Write-Host "- Failed to get release info from $textRepo!"
+    exit 1
+}
+
+$latestUploadDate = [datetime]::MinValue
+$downloadUrl = $null
+
+foreach ($release in $releaseInfo) {
+    foreach ($asset in $release.assets) {
+        if ($asset.name -like "*.zip" -and [datetime]$asset.updated_at -gt $latestUploadDate) {
+            $latestUploadDate = [datetime]$asset.updated_at
+            $downloadUrl = $asset.browser_download_url
+        }
+    }
+}
+
+if ($downloadUrl -eq $null) {
+    Write-Host "- Failed to find the latest release in $textRepo!"
+    exit 1
+}
+
+Write-Host "- Downloading latest from $textRepo..."
+$outputFile = "ra2yrbf_latest.zip"
+Invoke-WebRequest -Uri $downloadUrl -OutFile $outputFile
+
+if (-Not (Test-Path $outputFile)) {
+    Write-Host "- Failed to download the latest release from $textRepo!"
+    exit 1
+}
+
+Write-Host "- Download complete!"
+
+exit 0
